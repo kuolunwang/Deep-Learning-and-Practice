c35424922373661a5fa908a321b4caf2703da71b
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2021-05-28 03:18:11 +0800

    dataset

--- Lab7/dataset/__init__.py
+++ Lab7/dataset/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from .iCLEVR.iCLEVR import iCLEVRDataset
 from .CelebAHQ.CelebAHQ import CelebAHQDataset
 
--- Lab7/dataset/iCLEVR/iCLEVR.py
+++ Lab7/dataset/iCLEVR/iCLEVR.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import json
 import torch
 from torch.utils import data
