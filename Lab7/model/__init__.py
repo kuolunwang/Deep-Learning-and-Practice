1e57bc11dd26d05797b08e49a6c8e107789eadfd
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2021-06-12 17:44:49 +0800

    add glow model

---
+++ Lab7/model/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from model.glow import Glow
+from model.ACGAN import Generator, Discriminator
+# from model.CGAN import Generator, Discriminator
---
+++ Lab7/model/glow/__init__.py
@@ -0,0 +1,1 @@
+from model.glow.glow import Glow
