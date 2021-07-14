42f12977844bd47782c9fb9f1f02d8c921aa4bd8
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2021-07-14 18:19:08 +0800

    update torch to 1.9.0 and cuda

--- Lab4/install.sh
+++ Lab4/install.sh
@@ -9,6 +9,8 @@ virtualenv ${virtualenv_name}
 
 source ${VIRTUALENV_FOLDER}/bin/activate
 python3 -m pip install -r requirements.txt
+python3 -m pip install \
+    torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 deactivate
 echo "alias DLP_Lab4='source ${VIRTUALENV_FOLDER}/bin/activate '" >> ~/.bashrc
 source ~/.bashrc
